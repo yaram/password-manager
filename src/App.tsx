e4e59d3cc063cfa45762ecef74638ab987bde654
Yaram <pootisspencerherenao@gmail.com>
2019-03-02 15:44:54 +1100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return <div id='App'>
+            Test
+        </div>;
+    }
+}
+
+export default App;
